f6008ecdc6047b2fe234ae4f37c6331819a52b85
zohaibalidahri797-png <zohaibalidahri797@gmail.com>
2026-09-02 17:37:36 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig(() => {
+  return {
+    base: '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      hmr: process.env.DISABLE_HMR !== 'true',
+      watch: process.env.DISABLE_HMR === 'true' ? null : {},
+    },
+  };
+});
